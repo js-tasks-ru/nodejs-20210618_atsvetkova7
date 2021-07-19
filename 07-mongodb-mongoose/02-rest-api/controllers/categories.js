4d17012237ec50bbc5a6b6f88942776857889299
Anastasiia Tsvetkova <atsvetkova7@gmail.com>
2021-07-19 20:51:10 +0300

    Add solution for 7-module 2-task

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,23 @@
+const Category = require('../models/Category');
+
+const toSubcategoryDTO = (model) => {
+  return {
+    id: model._id,
+    title: model.title,
+  };
+};
+
+const toCategoryDTO = (model) => {
+  return {
+    id: model._id,
+    title: model.title,
+    subcategories: model.subcategories.map(toSubcategoryDTO),
+  };
+};
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({});
+  const categoryDTO = categories.map(toCategoryDTO);
+  ctx.body = {categories: categoryDTO};
+  await next();
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,60 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+const toProductDTO = (model) => {
+  return {
+    id: model._id,
+    title: model.title,
+    images: model.images,
+    category: model.category,
+    subcategory: model.subcategory,
+    price: model.price,
+    description: model.description,
+  };
+};
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategoryId = ctx.query.subcategory;
+  if (mongoose.isValidObjectId(subcategoryId)) {
+    const productBySubcategory = await Product.find({subcategory: subcategoryId});
+    if (productBySubcategory.length) {
+      ctx.body = {
+        products: productBySubcategory.map(toProductDTO),
+      };
+    } else {
+      await next();
+    }
+  } else {
+    ctx.throw(400, 'Invalid id.');
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+  if (products.length) {
+    ctx.body = {
+      products: products.map(toProductDTO),
+    };
+  } else {
+    ctx.body = {
+      products: [],
+    };
+  }
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const productID = ctx.params.id;
+  if (mongoose.isValidObjectId(productID)) {
+    const productById = await Product.findById(productID);
+    if (productById) {
+      ctx.body = {
+        product: toProductDTO(productById),
+      };
+    } else {
+      ctx.throw(404, 'Product was not found.');
+    }
+  } else {
+    ctx.throw(400, 'Invalid id.');
+  }
 };
 
